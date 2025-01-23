9154e29bdacba152106bcd349df592d969b66a77
benyamin <benyaminshany@outlook.com>
2025-01-23 12:51:08 +0200

    added to meet class and baseactivity class

--- app/src/main/java/com/example/benyaminbagrutproject/BaseActivity.java
+++ app/src/main/java/com/example/benyaminbagrutproject/BaseActivity.java
@@ -1,5 +1,81 @@
 package com.example.benyaminbagrutproject;
 
-public class BaseActivity {
+public class BaseActivity
+{
+    protected String activityID  ,creatorID , meetID;
+    protected String title;
+
+    protected String type;
+    protected int Date,month,year;
+
+
+    public BaseActivity() {
+    }
+
+    public String getActivityID() {
+        return activityID;
+    }
+
+    public void setActivityID(String activityID) {
+        this.activityID = activityID;
+    }
+
+    public String getCreatorID() {
+        return creatorID;
+    }
+
+    public void setCreatorID(String creatorID) {
+        this.creatorID = creatorID;
+    }
+
+    public String getMeetID() {
+        return meetID;
+    }
+
+    public void setMeetID(String meetID) {
+        this.meetID = meetID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDate() {
+        return Date;
+    }
+
+    public void setDate(int date) {
+        Date = date;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 
 }
+
--- app/src/main/java/com/example/benyaminbagrutproject/Meet.java
+++ app/src/main/java/com/example/benyaminbagrutproject/Meet.java
@@ -1,18 +1,23 @@
 package com.example.benyaminbagrutproject;
 
 
+import java.util.ArrayList;
+
 public class Meet {
     protected String name;
     protected String meetID;
 
-    /* add date variable here , with getter and setter */
+    protected int Date,month,year;
 
+    protected ArrayList<String> activityIDs;
+
+    //
 
     public Meet() {
     }
 
 
-
+    //
 
     public String getName() {
         return name;
@@ -29,5 +34,43 @@ public class Meet {
 
     public void setMeetID(String meetID) {
         this.meetID = meetID;
+    }
+
+
+    //
+
+
+    public int getDate() {
+        return Date;
+    }
+
+    public void setDate(int date) {
+        Date = date;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    //
+
+    public ArrayList<String> getActivityIDs() {
+        return activityIDs;
+    }
+
+    public void setActivityIDs(ArrayList<String> activityIDs) {
+        this.activityIDs = activityIDs;
     }
 }
