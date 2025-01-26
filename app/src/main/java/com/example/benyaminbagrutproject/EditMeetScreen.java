2c97e79ce6148c8397f6c9c2d59b211cd79cc40d
benyamin <benyaminshany@outlook.com>
2025-01-26 14:16:57 +0200

    added meet editing screen , made layout for meets listview  and activities listview

---
+++ app/src/main/java/com/example/benyaminbagrutproject/EditMeetScreen.java
@@ -0,0 +1,16 @@
+package com.example.benyaminbagrutproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditMeetScreen extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meet_edit_screen);
+    }
+}
